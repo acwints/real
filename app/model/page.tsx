697c6d72352dfc84ffefc113d61a0c7445fb826f
Andrew Winter <acwints78@gmail.com>
2025-11-27 12:15:28 +1100

    Fix syntax error - close fragment properly

--- app/model/page.tsx
+++ app/model/page.tsx
@@ -116,6 +116,7 @@ export default function ModelPage() {
                 </div>
               </div>
             </div>
+            </>
           )}
 
           {activeView === 'formatted' && (
